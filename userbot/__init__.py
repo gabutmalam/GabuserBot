9ce45741957cc2ed12968962448865f4c7e7a976
gabutmalam <85965690+gabutmalam@users.noreply.github.com>
2021-06-20 12:59:42 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -108,7 +108,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/gabutmalam/GabuseBot")
+    "https://github.com/gabutmalam/GabuserBot")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "RAM-UBOT")
 
