07c4739f96be2b40ec8c599b907426541777d12e
Saskeh <85965690+gabutmalam@users.noreply.github.com>
2021-06-29 08:00:54 +0700

    update sangmata

--- userbot/modules/sangmata.py
+++ userbot/modules/sangmata.py
@@ -16,16 +16,16 @@ async def lastname(steal):
     if steal.fwd_from:
         return
     if not steal.reply_to_msg_id:
-        await steal.edit("```Mohon Reply Ke Pesan Pengguna Dulu ngentot```")
+        await steal.edit("Mohon Reply Ke Pesan Pengguna Dahulu ya..")
         return
     message = await steal.get_reply_message()
     chat = "@SangMataInfo_bot"
     user_id = message.sender.id
     id = f"/search_id {user_id}"
     if message.sender.bot:
-        await steal.edit("```Reply Ke Pesan Pengguna dulu ngentot```")
+        await steal.edit("Reply Ke Pesan Pengguna Dahulu ya..")
         return
-    await steal.edit("LU SIAPA SI KENTOT,GUA KEPO, GA SENENG PC!!.")
+    await steal.edit("Who Are you?..")
     try:
         async with bot.conversation(chat) as conv:
             try:
@@ -47,7 +47,7 @@ async def lastname(steal):
             if response.text.startswith("No records") or r.text.startswith(
                 "No records"
             ):
-                await steal.edit("```Masa Gua Gak Nemu Riwayat Namanya si ngentot, Wah si anjing belom pernah ganti nama kayak nya su!!```")
+                await steal.edit("riwayat tidak ditemukan..")
                 await steal.client.delete_messages(
                     conv.chat_id, [msg.id, r.id, response.id]
                 )
@@ -64,6 +64,6 @@ async def lastname(steal):
 
 CMD_HELP.update({
     "sangmata":
-        "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `.sa`\
-          \n↳ : Mendapatkan Riwayat Nama Pengguna Yang Di Scan."
+        "**Command**: `.sa`\
+          \n✘ : Mendapatkan Riwayat Nama Pengguna Yang Di Scan."
 })
