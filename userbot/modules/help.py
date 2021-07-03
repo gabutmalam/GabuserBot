4f36bfd4903d2d9e382a90dce868dd443bd0f9ae
Saskeh <85965690+gabutmalam@users.noreply.github.com>
2021-07-03 09:19:14 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -25,7 +25,7 @@ async def help(event):
         if args in CMD_HELP:
             await event.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("**NGETIK YG BENER YA GANTENG...**")
+            await event.edit("__Your Command Wrong Sir!__")
             await asyncio.sleep(100)
             await event.delete()
     else:
@@ -34,9 +34,9 @@ async def help(event):
             string += "`" + str(i)
             string += "`\t🐋  "
         await event.edit(f"**{REPO_NAME}**\n\n"
-                         f"**🐋 𝙿𝙴𝙼𝙸𝙻𝙸𝙺 𝙱𝙾𝚃 : {DEFAULTUSER}**\n**🐋  𝙼𝙾𝙳𝚄𝙻𝙴𝚂 : {len(modules)}**\n\n"
-                         "**🐋 𝚂𝙴𝙼𝚄𝙰 𝙼𝙴𝙽𝚄 :**\n\n ══════════╣❃ ♕ ❃╠══════════\n\n"
-                         f"🐋 {string}\n\n ══════════╣❃ ♕ ❃╠══════════\n\nNGETIK YANG BENER YA GANTENG...\n\n")
+                         f"**🐋 Owner Bot : Tn.{DEFAULTUSER}**\n**🐋  𝙼𝙾𝙳𝚄𝙻𝙴𝚂 : {len(modules)}**\n\n"
+                         "**🐋 All Module :**\n\n ══════════╣❃ ♕ ❃╠══════════\n\n"
+                         f"🐋 {string}\n\n ══════════╣❃ ♕ ❃╠══════════\n\n__Your Command Wrong Sir!__\n\n")
         await event.reply(f"\n**Contoh** : Ketik .help ping Untuk Informasi Pengunaan.\nJangan Lupa Berdoa Sebelum Mencoba Mwehehehehe...")
         await asyncio.sleep(50)
         await event.delete()
